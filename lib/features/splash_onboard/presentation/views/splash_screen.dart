61510868064a19d27ffc703280fb686597259361
karim33elabasy <karim33elabasy@gmail.com>
2024-07-17 00:37:55 +0300

    to save

--- lib/features/splash_onboard/presentation/views/splash_screen.dart
+++ lib/features/splash_onboard/presentation/views/splash_screen.dart
@@ -1,14 +1,33 @@
 import 'package:dentister/core/utils/app_assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 3),
+        (){
+
+        }
+    );
+  }
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Image.asset(AppAssets),
+        child: Image.asset(
+          AppAssets.logo,
+          width: MediaQuery.sizeOf(context).width*0.7,
+        ),
+
       ),
     );
   }
