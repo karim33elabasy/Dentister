ddf9b59ec0051533b09b24db47b7ee144ff88bbb
karim33elabasy <karim33elabasy@gmail.com>
2024-07-17 00:25:00 +0300

    to save

--- lib/features/splash_onboard/presentation/views/splash_screen.dart
+++ lib/features/splash_onboard/presentation/views/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:dentister/core/utils/app_assets.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -6,7 +7,9 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: AppAss,
+      body: Center(
+        child: Image.asset(AppAssets),
+      ),
     );
   }
 }
