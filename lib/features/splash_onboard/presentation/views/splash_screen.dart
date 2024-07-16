33aaffaaf958f7ac83f7c97a4b05d8f31ac45988
karim33elabasy <karim33elabasy@gmail.com>
2024-07-17 00:44:15 +0300

    built splash_screen.dart

--- lib/features/splash_onboard/presentation/views/splash_screen.dart
+++ lib/features/splash_onboard/presentation/views/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:dentister/core/utils/app_assets.dart';
+import 'package:dentister/core/utils/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -15,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
     Future.delayed(
       const Duration(seconds: 3),
         (){
-
+          context.go(AppRouter.kHomeScreen);
         }
     );
   }
