827f9d051d415d81b16097dd48f91703d8630fd6
karim33elabasy <karim33elabasy@gmail.com>
2024-07-17 00:15:56 +0300

    to save

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 abstract class AppRouter{
-  static Route
+  static GoRouter router = GoRouter(
+      routes: [
+
+      ]
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dentister/core/utils/app_router.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,7 @@ class Dentister extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: ,
+      routerConfig: AppRouter.route,
     );
   }
 }
