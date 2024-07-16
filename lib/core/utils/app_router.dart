c10ddf9ea921cf2eadd3a1a5f0dd167fc713e3ed
karim33elabasy <karim33elabasy@gmail.com>
2024-07-17 00:37:55 +0300

    to save

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -3,11 +3,17 @@ import 'package:go_router/go_router.dart';
 
 abstract class AppRouter{
   static String kSplashScreen = "/";
+  static String kHomeScreen = "kHomeScreen";
+
   static GoRouter router = GoRouter(
       routes: [
         GoRoute(
           path: kSplashScreen,
           builder: (context,state)=>const SplashScreen()
+        ),
+        GoRoute(
+            path: kHomeScreen,
+            builder: (context,state)=>const HomeScree()
         ),
       ]
   );
