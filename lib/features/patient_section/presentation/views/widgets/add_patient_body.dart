0ae056518195d67adef440f6fcac5e52429abbde
karim33elabasy <karim33elabasy@gmail.com>
2024-07-17 18:41:39 +0300

    to save

--- lib/features/patient_section/presentation/views/widgets/add_patient_body.dart
+++ lib/features/patient_section/presentation/views/widgets/add_patient_body.dart
@@ -1,8 +1,14 @@
+import 'package:flutter/material.dart';
+
 class AddPatientBody extends StatelessWidget {
   const AddPatientBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        
+      ],
+    );
   }
 }
