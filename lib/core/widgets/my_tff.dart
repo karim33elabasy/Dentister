923dfd59cdc3533c1803ff0eb427a3738ff00170
karim33elabasy <karim33elabasy@gmail.com>
2024-07-17 18:50:40 +0300

    to save

--- lib/core/widgets/my_tff.dart
+++ lib/core/widgets/my_tff.dart
@@ -2,13 +2,17 @@ import 'package:flutter/material.dart';
 
 class MyTff extends StatelessWidget {
   final bool obscureText;
-  const MyTff({super.key});
+  final String label;
+  final TextEditingController controller;
+  const MyTff({super.key, required this.obscureText, required this.label, required this.controller});
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       obscureText: obscureText,
+      controller: controller,
       decoration: InputDecoration(
+        label: Text(label),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
         ),
--- lib/features/patient_section/presentation/views/widgets/add_patient_body.dart
+++ lib/features/patient_section/presentation/views/widgets/add_patient_body.dart
@@ -1,3 +1,4 @@
+import 'package:dentister/core/widgets/my_tff.dart';
 import 'package:flutter/material.dart';
 
 class AddPatientBody extends StatelessWidget {
@@ -5,10 +6,16 @@ class AddPatientBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        
-      ],
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.sizeOf(context).width*0.045,
+        vertical: MediaQuery.sizeOf(context).height*0.02
+      ),
+      child: Column(
+        children: [
+          MyTff(obscureText:false,label: "Name",controller: TextEditingController(),)
+        ],
+      ),
     );
   }
 }
