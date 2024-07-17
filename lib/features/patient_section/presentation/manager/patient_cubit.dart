900d3754b080ae6a829558ebdbd07b6b2717410d
karim33elabasy <karim33elabasy@gmail.com>
2024-07-18 00:46:44 +0300

    built patient_cubit.dart and patient_repo_implem.dart and finished the patient_add_screen.dart

--- lib/features/patient_section/presentation/manager/patient_cubit.dart
+++ lib/features/patient_section/presentation/manager/patient_cubit.dart
@@ -1,5 +1,19 @@
+import 'package:bloc/bloc.dart';
+import 'package:dentister/features/patient_section/data/model/patient_model.dart';
 import 'package:dentister/features/patient_section/presentation/manager/patient_states.dart';
 
 class PatientCubit extends Cubit<PatientStates>{
+  PatientCubit():super(PatientStateInitial());
 
+  addNewPatient(PatientModel patient){
+    emit(PatientStateLoading());
+  }
+
+  editPatient(int patientId,PatientModel patient){
+    emit(PatientStateLoading());
+  }
+
+  deletePatient(int patientId){
+    emit(PatientStateLoading());
+  }
 }
