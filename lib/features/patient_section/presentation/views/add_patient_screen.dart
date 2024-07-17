e09a5088b04b577bb5739f8e462ac0b4ef40591b
karim33elabasy <karim33elabasy@gmail.com>
2024-07-17 17:57:29 +0300

    to save

---
+++ lib/features/patient_section/presentation/views/add_patient_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddPatientScreen extends StatelessWidget {
+  const AddPatientScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
