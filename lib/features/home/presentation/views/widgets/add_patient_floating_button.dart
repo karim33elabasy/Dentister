437af6e2efbf8e2745d488109b0894fd6ea76a13
karim33elabasy <karim33elabasy@gmail.com>
2024-07-17 17:57:29 +0300

    to save

---
+++ lib/features/home/presentation/views/widgets/add_patient_floating_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddPatientFloatingButton extends StatelessWidget {
+  const AddPatientFloatingButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+        onPressed: (){
+
+        }
+    );
+
+  }
+}
